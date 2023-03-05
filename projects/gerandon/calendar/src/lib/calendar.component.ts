1aee61f733e71d913bfe307fab6b88b6dfb6e50d
Asztalos Gergő <gergo0317@gmail.com>
2023-03-05 20:44:25 +0100

    initial commit 2

---
+++ projects/gerandon/calendar/src/lib/calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-calendar',
+  template: `
+    <p>
+      calendar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CalendarComponent {
+
+}
